4fe325118f84c6cea4bd39f6c033c120eec25d31
Zohaib Shahid <z-shaid@ejada.com>
2024-02-28 15:36:39 +0500

    feat: init project

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wjppptokfgaoncgyjhwh.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqcHBwdG9rZmdhb25jZ3lqaHdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDExNDIyNjksImV4cCI6MjAxNjcxODI2OX0.CNXgKm_OrvAuOB5HlmaFkGgj4X3imnJ9hbl6DBvF1Kw',
+};

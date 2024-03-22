9d90557c366964d5e1e959b23a15fccaea925229
zohaib <protechgiant@gmail.com>
2024-03-22 12:52:21 +0500

    feat: changes for analytics

--- src/feature/Home/screens/HomeScreen.tsx
+++ src/feature/Home/screens/HomeScreen.tsx
@@ -1,14 +1,13 @@
 import Page from "@/components/Page";
-import { CreateOrganization, OrganizationProfile, useOrganization } from "@clerk/clerk-react";
+import { useOrganization } from "@clerk/clerk-react";
 
 export default function HomeScreen() {
-  const { organization } = useOrganization()
-  console.log(organization?.createdAt);
-  
+  const { organization } = useOrganization();
+  console.log(organization?.slug);
+
   return (
     <Page>
-      <OrganizationProfile routing='path' path="/organization-profile" />
-      <CreateOrganization />
+      <></>
     </Page>
   );
 }
--- src/feature/Settings/screens/SettingsScreen.tsx
+++ src/feature/Settings/screens/SettingsScreen.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import Page from "@/components/Page";
-import { Divider, Grid, Stack, Typography, styled } from "@mui/material";
+import { Dialog, DialogContent, DialogContentText, DialogTitle, Divider, Grid, Stack, Typography, styled } from "@mui/material";
 import StoreDisplayComponent from "../components/StoreDisplayComponent";
 import { useTranslation } from "react-i18next";
 import Button from "@/components/Button";
 import { DeleteIcon } from "@/components/icons";
 import { useDeleteStore } from "../hooks/queryHooks";
+import DialogAction from "@/components/Dialog/DialogActions";
 
 export default function SettingsScreen() {
   const { t } = useTranslation();
   const { mutateAsync, isPending: isStoreDeleteLoading } = useDeleteStore();
+  const [openDialog, setOpenDialog] = useState(true);
 
   const handleStoreDelete = async () => {
     await mutateAsync();
     window.location.reload();
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
   };
 
   return (
@@ -28,13 +39,22 @@ export default function SettingsScreen() {
             </Typography>
             <Stack flexDirection={"row"} alignItems={"end"} justifyContent={"space-between"}>
               <Typography variant='subtitle1'>{t("delete")}</Typography>
-              <Button color='error' startIcon={<DeleteIcon />} onClick={handleStoreDelete} loading={isStoreDeleteLoading}>
+              <Button color='error' startIcon={<DeleteIcon />} onClick={handleOpenDialog} loading={isStoreDeleteLoading}>
                 {t("delete")}
               </Button>
             </Stack>
           </Grid>
         </Grid>
       </Stack>
+      <Dialog open={openDialog} onClose={handleCloseDialog} maxWidth='sm' fullWidth>
+        <DialogTitle>Alert - changes will not effect on organization</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Alert - changes will not effect on organization because our team is working on it</DialogContentText>
+        </DialogContent>
+        <DialogAction>
+          <Button onClick={handleCloseDialog}>{t("close")}</Button>
+        </DialogAction>
+      </Dialog>
     </Page>
   );
 }

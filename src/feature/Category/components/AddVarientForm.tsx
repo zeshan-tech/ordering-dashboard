427806d40f6b3df05e96d46f6e4e854ee334f7a1
zohaib <protechgiant@gmail.com>
2024-03-24 14:18:30 +0500

    feat: changes for buld

--- src/feature/Category/components/AddVarientForm.tsx
+++ src/feature/Category/components/AddVarientForm.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"; // Import useState
+// Import necessary dependencies
+import { useState } from "react";
 import { useForm, SubmitHandler, useFieldArray, Controller } from "react-hook-form";
 import { TextField, Typography, Stack, Toolbar, Card, CardContent, MenuItem, CardActions } from "@mui/material";
 import { Form } from "@/components/Form";
@@ -19,71 +20,79 @@ interface AddVariantFormProps {
   productId: string;
 }
 
+// Component function
 export default function AddVariantForm({ productId }: AddVariantFormProps) {
+  // Hooks and state
   const { t } = useTranslation();
   const navigation = useNavigation();
-
   const [imageUrls, setImageUrls] = useState<string[][]>([[]]);
-
   const { mutateAsync } = useAddVariant();
-
   const { control, handleSubmit } = useForm<IFormValues>({});
-
   const { fields, append, remove } = useFieldArray({
     control,
     name: "variants",
   });
 
-  // State for storing image URLs
-
+  // Form submission handler
   const onSubmit: SubmitHandler<IFormValues> = (data) => {
     const formData = data.variants.map((v, i) => {
-      v["imageUrls"] = imageUrls[i];
+      // v["imageUrls"] = imageUrls[i];
       return v;
     });
-
     console.log(formData);
   };
 
-  console.log(imageUrls);
-  
-
+  // JSX return
   return (
     <Form component={Card} onSubmit={handleSubmit(onSubmit)}>
       <Toolbar>
         <Typography variant='h5' sx={{ flexGrow: 1 }}>
-          Add variant
+          {t("addVariant")}
         </Typography>
-        <Button onClick={() => append({ type: VariantTypeEnum.MATERIAL, value: "", price: 0, imageUrls: [], product: productId })}>Add Variant</Button>
+        <Button onClick={() => append({ type: VariantTypeEnum.MATERIAL, value: "", price: 0, imageUrls: [], product: productId })}>{t("addVariant")}</Button>
       </Toolbar>
       <CardContent component={Stack} spacing={2}>
-        {fields.map((field, index) => (
-          <Stack key={field.id} gap={1}>
-            <Stack key={field.id} flexDirection={"row"} gap={1}>
-              <Controller
-                name={`variants.${index}.type`}
-                control={control}
-                render={({ field }) => (
-                  <TextField label='Type' select {...field} fullWidth>
-                    <MenuItem value={VariantTypeEnum.MATERIAL}>Material</MenuItem>
-                    <MenuItem value={VariantTypeEnum.SIZE}>Size</MenuItem>
-                    <MenuItem value={VariantTypeEnum.COLOR}>Color</MenuItem>
-                  </TextField>
-                )}
-              />
+        {fields.map((field, index) => {
+          console.log(imageUrls[index]);
+          return (
+            <Stack key={field.id} gap={1}>
+              <Stack key={field.id} flexDirection={"row"} gap={1}>
+                <Controller
+                  name={`variants.${index}.type`}
+                  control={control}
+                  render={({ field }) => (
+                    <TextField label={t("type")} select {...field} fullWidth>
+                      <MenuItem value={VariantTypeEnum.MATERIAL}>{t("material")}</MenuItem>
+                      <MenuItem value={VariantTypeEnum.SIZE}>{t("size")}</MenuItem>
+                      <MenuItem value={VariantTypeEnum.COLOR}>{t("color")}</MenuItem>
+                    </TextField>
+                  )}
+                />
 
-              <Controller name={`variants.${index}.value`} control={control} defaultValue='' render={({ field }) => <TextField {...field} label='Value' fullWidth />} />
+                <Controller name={`variants.${index}.value`} control={control} defaultValue='' render={({ field }) => <TextField {...field} label={t("value")} fullWidth />} />
 
-              <Controller name={`variants.${index}.price`} control={control} defaultValue={0} render={({ field }) => <TextField {...field} type='number' label='Price Adjustment' fullWidth />} />
+                <Controller name={`variants.${index}.price`} control={control} defaultValue={0} render={({ field }) => <TextField {...field} type='number' label={t("priceAdjustment")} fullWidth />} />
 
-              <UploadWidget component={<AddImageIcon solid />} onUpload={(url) => setImageUrls((prev) => [...prev, [...prev[index], url]])} />
+                {/* <UploadWidget component={<AddImageIcon solid />} onUpload={(url) => setImageUrls((prev) => [...prev, [...prev[index], url]])} /> */}
 
-              <DeleteIcon solid onClick={() => remove(index)} />
+                <UploadWidget
+                  component={<AddImageIcon solid />}
+                  onUpload={(url) => {
+                    setImageUrls((prev) => {
+                      const updatedUrls = [...prev];
+                      updatedUrls[index] = [...(prev[index] ?? []), url];
+                      return updatedUrls;
+                    });
+                  }}
+                />
+
+                <DeleteIcon solid onClick={() => remove(index)} />
+              </Stack>
+
+              <ProductImageList list={imageUrls[index] ?? []} />
             </Stack>
-
-            <ProductImageList list={imageUrls[index]} />
-          </Stack>
-        ))}
+          );
+        })}
       </CardContent>
       <CardActions>
         <Button onClick={navigation.goBack}>{t("skip")}</Button>
